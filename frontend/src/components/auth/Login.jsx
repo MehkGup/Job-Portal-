a8ddc360c5a009c2a4ea1ffeff22da57833773de
Mehk <166231141+MehkGup@users.noreply.github.com>
2026-05-04 11:12:49 +0530

    Set default role to 'student' in Login component

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
   const [input, setInput] = useState({
     email: "",
     password: "",
-    role: "",
+    role: "student",
   });
   const { loading, user } = useSelector(store => store.auth)
   const navigate = useNavigate();
