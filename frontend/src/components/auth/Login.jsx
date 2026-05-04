23504a39bd9473f868c35c034e8d29b42df6d269
Mehk <166231141+MehkGup@users.noreply.github.com>
2026-05-04 10:55:27 +0530

    Improve error handling in login

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
       }
     } catch (error) {
       console.log(error);
-      toast.error(error.response.data.message);
+     toast.error(error?.response?.data?.message || "Login Failed");
     } finally {
       dispatch(setLoading(false));
     }
